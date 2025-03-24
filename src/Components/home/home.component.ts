53cd2743d51c2a7438e9a88aa778d742484d112a
mohamedsherif301 <myosre122@gmail.com>
2025-03-24 12:41:15 +0200

    open in new tap social links in home Component

--- src/Components/home/home.component.ts
+++ src/Components/home/home.component.ts
@@ -9,5 +9,18 @@ import { SKilsComponent } from '../skils/skils.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  DetermineToLink(params: string, url: string) {
+    switch (params) {
+      case "linkedin":
+      case "github":
+      case "facebook":
+      case "youtube":
+        window.open(url, '_blank');
+        break;
+      default:
+        window.open(url, '_blank');
+        break;
+    }
+  }
 
 }
